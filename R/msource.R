6d58977f301b02ad956aacfe7bda375391d9826e
David J. Bosak <dbosak01@gmail.com>
2025-09-30 23:31:25 -0400

    Update docs

--- R/msource.R
+++ R/msource.R
@@ -154,6 +154,55 @@ strs <- paste0(rep("*", 80), collapse = "")
 #' @importFrom utils capture.output
 #' @returns The results of the \code{source()} function, invisibly.  The path
 #' of the resolved output file is also included under the "$output" list item.
+#' @examples
+#'
+#' # Create temp file
+#' tmp <- file.path(tempdir(), "test.R")
+#' out <- file.path(tempdir(), "test_mod.R")
+#'
+#' # Write macro code to temp file
+#' cat("#%let a <- 1\n", file = tmp)
+#' cat("#%if (a. == 1)\n", file = tmp, append = TRUE)
+#' cat("print('a is one!')\n", file =tmp, append = TRUE)
+#' cat("#%else\n", file = tmp, append = TRUE)
+#' cat("print('a is something else!')\n", file = tmp, append = TRUE)
+#' cat("#%end", file = tmp, append = TRUE)
+#' cat("\n", file = tmp, append = TRUE)
+#'
+#' # Process temp file
+#' res <- msource(tmp, out)
+#' # [1] "a is one!"
+#'
+#' # Read output file
+#' readLines(res$output)
+#' # [1] "print('a is one!')"
+#'
+#' # Rerun in debugger
+#' msource(tmp, out, debug = TRUE)
+#' # ********************************************************************************
+#' # **  Pre-Processing
+#' # ********************************************************************************
+#' # -    File In: C:\Users\dbosa\AppData\Local\Temp\RtmpyM9Kep/test.R
+#' # -   File Out: C:\Users\dbosa\AppData\Local\Temp\RtmpyM9Kep/test_mod.R
+#' # ********************************************************************************
+#' # [ In#][Out#]:
+#' # [   1][    ]: #%let a <- 1
+#' # [   2][    ]: #%if (a. == 1)
+#' # [   3][   1]: print('a is one!')
+#' # [   4][    ]: #%else
+#' # [   5][    ]: print('a is something else!')
+#' # [   6][    ]: #%end
+#' # ********************************************************************************
+#' # **  Execution
+#' # ********************************************************************************
+#' #
+#' # > print('a is one!')
+#' # [1] "a is one!"
+#' #
+#' # ********************************************************************************
+#' # **  End
+#' # ********************************************************************************
+#'
 #' @export
 msource <- function(pth = Sys.path(), file_out = NULL, envir = globalenv(),
                     exec = TRUE, debug = FALSE, debug_out = NULL,
